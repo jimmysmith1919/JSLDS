a332a853c5477c180c169f104b65e8633b1e538f
Jimmy Smith <jimmysmith1919@gmail.com>
2021-02-14 11:17:22 -0800

    zero out kl ii loss

--- computation-thru-dynamics/lfads_tutorial/lfads_autonomous.py
+++ computation-thru-dynamics/lfads_tutorial/lfads_autonomous.py
@@ -652,7 +652,7 @@ def lfads_losses(params, lfads_hps, key, x_bxt, kl_scale, keep_rate):
                                           ii_post_var_bxt, params['ii_prior'],
                                           lfads_hps['var_min'])
   kl_loss_ii_prescale = np.sum(kl_loss_ii_b) / B
-  kl_loss_ii = kl_scale * kl_loss_ii_prescale
+  kl_loss_ii = 0*kl_scale * kl_loss_ii_prescale
   
   # Log-likelihood of data given latents nl_RNN
   lograte_bxt = lfads['lograte_t']
