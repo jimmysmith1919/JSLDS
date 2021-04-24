7df7ac765a4fd10f7e65613457f0c52bea92b306
Jimmy Smith <jimmysmith1919@gmail.com>
2021-04-24 10:56:52 -0700

    try to fix hash issue

--- computation-thru-dynamics/lfads_tutorial/lfads_gauss.py
+++ computation-thru-dynamics/lfads_tutorial/lfads_gauss.py
@@ -755,10 +755,18 @@ def posterior_sample_and_average(params, lfads_hps, key, x_txd):
 # The static_argnums is telling JAX to ignore the lfads_hps dictionary,
 # which means you'll have to pay attention if you change the params.
 # How does one force a recompile?
+
+"
 batch_lfads_jit = jit(batch_lfads, static_argnums=(1,))
 lfads_losses_jit = jit(lfads_losses, static_argnums=(1,))
 lfads_training_loss_jit = jit(lfads_training_loss, static_argnums=(1,))
 posterior_sample_and_average_jit = jit(posterior_sample_and_average, static_argnums=(1,))
+"
+
+batch_lfads_jit = jit(batch_lfads)
+lfads_losses_jit = jit(lfads_losses)
+lfads_training_loss_jit = jit(lfads_training_loss)
+posterior_sample_and_average_jit = jit(posterior_sample_and_average)
 
                   
   
